29d3dc9187a5ba1d42fbd356c78dace06a73fb03
Simon Renoult <simon.renoult@gmail.com>
2018-03-15 17:45:37 +0100

    fix linter errors

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-
 const express = require('express');
-
-const package = require('./package.json');
+const pkg = require('./package.json');
 
 const app = express();
 const config = {
-    PORT: (process.env.PORT || 3000)
+  PORT: (process.env.PORT || 3000),
 };
 
 app.get('/', (req, res) => {
-    const json = { name: package.name, version: package.version, author: package.author };
-    res.send(json);
+  const json = { name: pkg.name, version: pkg.version, author: pkg.author };
+  res.send(json);
 });
 
 if (require.main === module) {
-    // the file is run directly, run the server
-    console.log(`Listing on port ${config.PORT}`);
-    app.listen(config.PORT);
+  // the file is run directly, run the server
+  console.log(`Listing on port ${config.PORT}`);
+  app.listen(config.PORT);
 }
 
 module.exports = {
-    app, config
-}
+  app, config,
+};
--- tests/integration/server_test.js
+++ tests/integration/server_test.js
@@ -1,14 +1,13 @@
-
 const request = require('supertest');
 const { describe, it } = require('mocha');
 
 const { app } = require('../../server');
 
-describe('GET /', function() {
-    it('respond with api version', function(done) {
-        request(app)
-        .get('/')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
-    });
+describe('GET /', () => {
+  it('respond with api version', (done) => {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
 });
