2b80e4473428cc88c6cd261dde672d189d416802
Simon Renoult <simon.renoult@gmail.com>
2018-03-16 16:04:38 +0100

    split spreadsheet controller and use promises

--- src/repositories/spreadsheet.js
+++ src/repositories/spreadsheet.js
@@ -1,38 +1,63 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
 const moment = require('moment');
 
+function getSpreadsheets(config) {
+  return new Promise((resolve, reject) => {
+    const doc = new GoogleSpreadsheet(config.id);
+    doc.useServiceAccountAuth(config.credentials, (authError) => {
+      if (authError) return reject(authError);
+      return doc.getInfo((getInfoError, spreadsheets) => {
+        if (getInfoError) return reject(getInfoError);
+        return resolve(spreadsheets);
+      });
+    });
+  });
+}
+
+function findCell(inscriptionWorksheet) {
+  const DATE_CELL_POSITION = {
+    'min-row': 3,
+    'max-row': 3,
+    'min-col': 1,
+    'max-col': 1,
+  };
+
+  return new Promise((resolve, reject) => {
+    inscriptionWorksheet.getCells(DATE_CELL_POSITION, (getCellsError, cells) => {
+      if (getCellsError) return reject(getCellsError);
+      return resolve(cells[0]);
+    });
+  });
+}
+
 function parseDate(rawFrenchDate) {
   moment.locale('fr');
   return moment(rawFrenchDate, 'DD MMMM YYYY').format('YYYY-MM-DD');
 }
 
-function getNextBofDate(spreadsheetConfig) {
-  const INSCRIPTION_WORKSHEET_INDEX = 1;
+function getNextBofDate(config) {
+  return async () => {
+    let spreadsheets;
+    try {
+      spreadsheets = await getSpreadsheets(config);
+    } catch (e) {
+      return Promise.reject(e);
+    }
 
-  return () => new Promise((resolve, reject) => {
-    const doc = new GoogleSpreadsheet(spreadsheetConfig.id);
-    doc.useServiceAccountAuth(spreadsheetConfig.credentials, (authError) => {
-      if (authError) reject(authError);
-      doc.getInfo((getInfoError, spreadsheets) => {
-        if (getInfoError) reject(getInfoError);
+    const INSCRIPTION_WORKSHEET_INDEX = 1;
+    const inscriptionWorksheet = spreadsheets.worksheets[INSCRIPTION_WORKSHEET_INDEX];
 
-        const dateCellPosition = {
-          'min-row': 3,
-          'max-row': 3,
-          'min-col': 1,
-          'max-col': 1,
-        };
+    let cell;
+    try {
+      cell = await findCell(inscriptionWorksheet);
+    } catch (e) {
+      return Promise.reject(e);
+    }
 
-        const inscriptionWorksheet = spreadsheets.worksheets[INSCRIPTION_WORKSHEET_INDEX];
+    const nextBofDate = parseDate(cell.value);
 
-        inscriptionWorksheet.getCells(dateCellPosition, (getCellsError, cells) => {
-          if (getCellsError) return reject(getCellsError);
-          const nextBofDate = parseDate(cells[0].value);
-          return resolve(nextBofDate);
-        });
-      });
-    });
-  });
+    return Promise.resolve(nextBofDate);
+  };
 }
 
 module.exports = config => ({
