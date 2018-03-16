9fc24a1da0cc49623b50c7176c00083156c4bd6d
Simon Renoult <simon.renoult@gmail.com>
2018-03-16 15:29:56 +0100

    extract boot checks to allow unit testing

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
 const express = require('express');
 const routes = require('./src/routes');
 
-if (!process.env.SPREADSHEET_ID) {
-  throw new Error("Environment variable 'SPREADSHEET_ID' must be provided");
-}
-
-if (!process.env.CLIENT_EMAIL) {
-  throw new Error("Environment variable 'CLIENT_EMAIL' must be provided");
-}
-
-if (!process.env.PRIVATE_KEY) {
-  throw new Error("Environment variable 'PRIVATE_KEY' must be provided");
-}
-
 const app = express();
 routes.setupAllRoutesInApp(app, routes.routes);
 
