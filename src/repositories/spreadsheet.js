9e57a0674da654d5c7d797ea73d9075ce7426a91
Simon Renoult <simon.renoult@gmail.com>
2018-03-19 18:50:39 +0100

    remove useless try/catch and use pify

--- src/repositories/spreadsheet.js
+++ src/repositories/spreadsheet.js
@@ -1,17 +1,10 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
+const pify = require('pify');
 const dateAdapter = require('../adapters/date');
 
 function getBofsDocument(documentId, serviceCredentials) {
-  return new Promise((resolve, reject) => {
-    const doc = new GoogleSpreadsheet(documentId);
-    doc.useServiceAccountAuth(serviceCredentials, (authError) => {
-      if (authError) return reject(authError);
-      return doc.getInfo((getInfoError, spreadsheets) => {
-        if (getInfoError) return reject(getInfoError);
-        return resolve(spreadsheets);
-      });
-    });
-  });
+  const doc = new GoogleSpreadsheet(documentId);
+  return pify(doc.useServiceAccountAuth)(serviceCredentials).then(() => pify(doc.getInfo)());
 }
 
 function findNextBofDateCell(inscriptionWorksheet) {
@@ -22,32 +15,16 @@ function findNextBofDateCell(inscriptionWorksheet) {
     'max-col': 1,
   };
 
-  return new Promise((resolve, reject) => {
-    inscriptionWorksheet.getCells(DATE_CELL_POSITION, (getCellsError, cells) => {
-      if (getCellsError) return reject(getCellsError);
-      return resolve(cells[0]);
-    });
-  });
+  return pify(inscriptionWorksheet.getCells)(DATE_CELL_POSITION).then(cells => cells[0]);
 }
 
 function getNextBofDate(config) {
   return async () => {
-    let doc;
-    try {
-      doc = await getBofsDocument(config.id, config.credentials);
-    } catch (e) {
-      return Promise.reject(e);
-    }
+    const doc = await getBofsDocument(config.id, config.credentials);
 
     const INSCRIPTION_WORKSHEET_INDEX = 1;
     const inscriptionWorksheet = doc.worksheets[INSCRIPTION_WORKSHEET_INDEX];
-
-    let cell;
-    try {
-      cell = await findNextBofDateCell(inscriptionWorksheet);
-    } catch (e) {
-      return Promise.reject(e);
-    }
+    const cell = await findNextBofDateCell(inscriptionWorksheet);
 
     const nextBofDate = dateAdapter.parseNextBofsDate(cell.value);
 
