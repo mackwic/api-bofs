4139a6ce8c40a6a955f0f27dbbf71cb2f09fc486
Thomas Wickham <mackwic@gmail.com>
2018-03-26 12:32:57 +0200

    try to show the version in /

--- src/controllers/root.js
+++ src/controllers/root.js
@@ -3,7 +3,7 @@ const { name, version, contributors } = require('../../package.json');
 module.exports = {
   get: (req, res) => {
     const json = {
-      name, version, contributors,
+      name, contributors, version: `${version}+${process.env.CONTAINER_VERSION}`,
     };
     return res.send(json);
   },
